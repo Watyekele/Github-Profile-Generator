6e3ca4dd200a05e3c85e82eeac3b564cd74e30ed
Watyekele <Naluume>
2023-11-20 02:19:42 +0300

    Added Tailwind-Css and Mantine configurations

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Profiledata from "./Components/Profiledata";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+function App() {
+  return;
+}
+export default App;
---
+++ src/Components/Profiledata.jsx
@@ -0,0 +1,4 @@
+function Profiledata() {
+  return;
+}
+export default Profiledata();
